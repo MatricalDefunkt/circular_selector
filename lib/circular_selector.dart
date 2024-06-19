92054f207d0ba57b459c2052b443d69ee7753bb4
MatricalDefunkt <pramitsmusic@gmail.com>
2024-06-19 18:10:26 +0530

    Add Circular Selector widget

--- lib/circular_selector.dart
+++ lib/circular_selector.dart
@@ -1,9 +1,289 @@
-library circular_selector;
+import 'package:flutter/material.dart';
+import 'dart:math';
 
-export 'circular_selector.dart';
+class CircularSelector extends StatefulWidget {
+  const CircularSelector({
+    super.key,
+    required this.children,
+    required this.childSize,
+    required this.radiusDividend,
+    required this.onSelected,
+    required this.customOffset,
+  });
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 2;
+  /// The number that the device width is divided by to
+  /// get the radius of the circle.
+  final double radiusDividend;
+
+  /// The children to be displayed in the circular selector.
+  final List<Widget> children;
+
+  /// The size of the children.
+  final double childSize;
+
+  /// The function to be called when a child is selected.
+  ///
+  /// The index of the selected child is passed as an argument.
+  final Function(int index) onSelected;
+
+  /// A custom offset defined from the top left for the circle.
+  final Offset customOffset;
+
+  static List<Container> getTestContainers(int num, double childSize) {
+    List<Container> containers = [];
+    for (int i = 0; i < num; i++) {
+      containers.add(Container(
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.green,
+        ),
+        child: Center(
+          child: Text(
+            (i + 1).toString(),
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ));
+    }
+    return containers;
+  }
+
+  static double calculateGestureAngle(
+      double dx, double dy, double xOrigin, double yOrigin) {
+    double angle = atan2(dy - yOrigin, dx - xOrigin) * 180 / pi;
+    angle = angle < 0 ? 360 + angle : angle;
+    angle = (angle + 90) % 360; // Adjusting the origin to 12 o'clock
+    return angle;
+  }
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CircularSelectorState createState() => _CircularSelectorState();
+}
+
+class _CircularSelectorState extends State<CircularSelector>
+    with SingleTickerProviderStateMixin {
+  late double rotation;
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  late double startRotation;
+  late Offset deviceSize;
+
+  @override
+  void initState() {
+    super.initState();
+    rotation = 0.0;
+    startRotation = 0.0;
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    )..addListener(() {
+        setState(() {
+          rotation = _animation.value;
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  int getTappedChildIndex(
+      double dx, double dy, double xOrigin, double yOrigin) {
+    final angleDeg =
+        CircularSelector.calculateGestureAngle(dx, dy, xOrigin, yOrigin);
+    final anglePerChild = 360 / widget.children.length;
+
+    // Adjust angle to account for current rotation
+    double adjustedAngle = angleDeg - (rotation * 180 / pi);
+    adjustedAngle = adjustedAngle < 0 ? 360 + adjustedAngle : adjustedAngle;
+
+    // Determine the child index based on the adjusted angle
+    int index =
+        (adjustedAngle / anglePerChild).round() % widget.children.length;
+    return index;
+  }
+
+  int getIndexAtTop() {
+    // Convert rotation from radians to degrees and normalize to 0-360 range
+    double rotationDegrees = (rotation * 180 / pi) % 360;
+    rotationDegrees =
+        rotationDegrees < 0 ? 360 + rotationDegrees : rotationDegrees;
+
+    // Calculate the angle per child
+    final anglePerChild = 360 / widget.children.length;
+
+    // Calculate the index of the child at the top
+    int indexAtTop = ((360 - rotationDegrees) / anglePerChild).round() %
+        widget.children.length;
+
+    return indexAtTop;
+  }
+
+  void animateToTop(int index) {
+    final anglePerChild = 360 / widget.children.length;
+    var targetRotation = -(index * anglePerChild) * pi / 180;
+    final currentRotationMod = rotation % (2 * pi);
+    var rotationDiff = (currentRotationMod - targetRotation) % (2 * pi);
+
+    // Shortest rotation direction
+    if (rotationDiff > pi) {
+      rotationDiff -= 2 * pi;
+    } else if (rotationDiff < -pi) {
+      rotationDiff += 2 * pi;
+    }
+
+    targetRotation = currentRotationMod - rotationDiff;
+
+    // Dynamic Duration Calculation
+    const double angularVelocity = 1.5; // Radians per second
+    final duration = Duration(
+        milliseconds: (rotationDiff.abs() / angularVelocity * 300).round());
+
+    _controller.duration = duration;
+
+    if (_controller.isAnimating) {
+      _controller.stop();
+    }
+
+    _animation =
+        Tween<double>(begin: currentRotationMod, end: targetRotation).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    )..addListener(() {
+            setState(() {
+              rotation = _animation.value;
+            });
+          });
+
+    _controller.forward(from: 0);
+  }
+
+  void _onPanStart(DragStartDetails details) {
+    if (_controller.isAnimating) {
+      _controller.stop();
+    }
+    final dx = details.localPosition.dx;
+    final dy = details.localPosition.dy;
+
+    final xOrigin = deviceSize.dx / 2;
+    final yOrigin = deviceSize.dy / 2;
+
+    startRotation =
+        CircularSelector.calculateGestureAngle(dx, dy, xOrigin, yOrigin);
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    final dx = details.localPosition.dx;
+    final dy = details.localPosition.dy;
+
+    if (dx <= 1.0 && dy <= 1.0) {
+      return;
+    }
+
+    final xOrigin = deviceSize.dx / 2;
+    final yOrigin = deviceSize.dy / 2;
+
+    final angle =
+        CircularSelector.calculateGestureAngle(dx, dy, xOrigin, yOrigin);
+
+    final angleDiff = angle - startRotation;
+
+    setState(() {
+      // Update the rotation
+      rotation += (angleDiff * pi / 180);
+      startRotation = angle;
+    });
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    final anglePerChild = 2 * pi / widget.children.length;
+    double closestRotation = (rotation / anglePerChild).round() * anglePerChild;
+
+    _controller.duration = const Duration(milliseconds: 100);
+
+    // If the rotation is close to a child, snap to that child
+    _animation = Tween<double>(begin: rotation, end: closestRotation).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    )..addListener(() {
+        setState(() {
+          rotation = _animation.value;
+        });
+      });
+
+    _controller.forward(from: 0);
+
+    widget.onSelected(getIndexAtTop());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceHeight =
+        MediaQuery.of(context).size.height - AppBar().preferredSize.height;
+    deviceSize = Offset(deviceWidth, deviceHeight);
+
+    Point getPosition(int index, double childSize, double? xOrigin,
+        double? yOrigin, double? radius) {
+      xOrigin ??= deviceWidth / 2;
+      yOrigin ??= deviceHeight / 2;
+      radius ??= min(deviceWidth, deviceHeight) / widget.radiusDividend;
+
+      // Calculate the position of the child
+      final angle = 2 * pi * index / widget.children.length + rotation;
+      final dx = xOrigin + radius * cos(-(angle - pi / 2)) - childSize / 2;
+      final dy = yOrigin + radius * sin(-(angle - pi / 2)) - childSize / 2;
+
+      return Point(dx, dy);
+    }
+
+    List<Widget> positionedChildren = [];
+    for (int i = 0; i < widget.children.length; i++) {
+      final position = getPosition(i, widget.childSize, null, null, null);
+      final child = widget.children[i];
+      final positionedChild = Positioned(
+        left: position.x.toDouble(),
+        bottom: position.y.toDouble(),
+        child: SizedBox(
+          width: widget.childSize,
+          height: widget.childSize,
+          child: child,
+        ),
+      );
+      positionedChildren.add(positionedChild);
+    }
+
+    return GestureDetector(
+      onTapUp: (details) {
+        final dx = details.localPosition.dx;
+        final dy = details.localPosition.dy;
+        final xOrigin = (deviceWidth / 2);
+        final yOrigin = (deviceHeight / 2);
+
+        final index = getTappedChildIndex(dx, dy, xOrigin, yOrigin);
+        widget.onSelected(index);
+        animateToTop(index);
+      },
+      onPanStart: _onPanStart,
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
+      child: Container(
+        height: deviceHeight,
+        width: deviceWidth,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.blue,
+        ),
+        child: Stack(
+          children: positionedChildren,
+        ),
+      ),
+    );
+  }
 }
--- test/circular_selector_test.dart
+++ test/circular_selector_test.dart
@@ -1,12 +1,99 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:circular_selector/circular_selector.dart';
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Circular Selector Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Circular Selector Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey circularSelectorGlobalKey = GlobalKey();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.blue,
+          ),
+          child: CircularSelector(
+            onSelected: (int index) {
+              if (kDebugMode) {
+                print('Selected: $index');
+              }
+            },
+            customOffset: const Offset(0.0, 30.0),
+            childSize: 50.0,
+            radiusDividend: 2.5,
+            key: circularSelectorGlobalKey,
+            children: CircularSelector.getTestContainers(8, 50.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('CircularSelector test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    // Verify initial state
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('2'), findsOneWidget);
+    expect(find.text('3'), findsOneWidget);
+    expect(find.text('4'), findsOneWidget);
+    expect(find.text('5'), findsOneWidget);
+    expect(find.text('6'), findsOneWidget);
+    expect(find.text('7'), findsOneWidget);
+    expect(find.text('8'), findsOneWidget);
+
+    final text1InitialPosition = tester.getCenter(find.text("1"));
+    final text4InitialPosition = tester.getCenter(find.text("4"));
+
+    // Tap on the '4' text
+    await tester.tapAt(tester.getCenter(find.text("4")));
+
+    // Allow the animation to proceed and intermediate frames to be processed
+    await tester.pump(const Duration(seconds: 1));
+    await tester.pump(const Duration(seconds: 1));
+
+    // Retrieve final positions after animation
+    final text1FinalPosition = tester.getCenter(find.text("1"));
+    final text4FinalPosition = tester.getCenter(find.text("4"));
+
+    // LABEL 1: Tests if the initial position of text1 is equal to the final position of text4.
+    expect(text1InitialPosition, equals(text4FinalPosition),
+        reason: 'Text positions are not equal');
+
+    // Tests if the initial position of text1 is not equal to the final position of text1.
+    expect(text1InitialPosition, isNot(equals(text1FinalPosition)),
+        reason: 'Text positions are equal');
+
+    // Tests if the initial position of text4 is not equal to the final position of text4.
+    expect(text4InitialPosition, isNot(equals(text4FinalPosition)),
+        reason: 'Text positions are equal');
+
+    // Passed all tests
   });
 }
